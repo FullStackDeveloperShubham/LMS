093beb172ca5ce4e62f3efea86e223bf5aad738e
FullStackDeveloperShubham <shubhamfullstackdeveloper@gmail.com>
2025-09-24 23:42:31 +0530

    CORS impliment

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import rateLimit from "express-rate-limit";
@@ -38,6 +39,24 @@ app.use((err, req, res, next) => {
     ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
   });
 });
+
+// CORS CONFIGURATION
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+      "Access-Control-Allow-Origin",
+      "device-remeber-token",
+    ],
+  })
+);
 
 // API ROUTES
 
