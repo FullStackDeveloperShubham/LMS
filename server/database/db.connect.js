9401ad3b835b2f1d52594f9b6a601d58c7a36c56
FullStackDeveloperShubham <shubhamfullstackdeveloper@gmail.com>
2025-09-25 22:14:39 +0530

    Database connection close error handling

--- server/database/db.connect.js
+++ server/database/db.connect.js
@@ -25,23 +25,64 @@ class DatabaseConnection {
   }
 
   async connect() {
-    if (!process.env.MONGO_URL) {
-      throw new Error("mongodb url is not defined in env file ");
-    }
-    const connectionOptions = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      maxPoolSize: 10,
-      serverSelectionTimeOutMS: 5000,
-      socketTimeoutMS: 45000,
-      family: 4,
-    };
+    try {
+      if (!process.env.MONGO_URL) {
+        throw new Error("mongodb url is not defined in env file ");
+      }
+      const connectionOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        maxPoolSize: 10,
+        serverSelectionTimeOutMS: 5000,
+        socketTimeoutMS: 45000,
+        family: 4,
+      };
 
-    if (process.env.NODE_ENV) {
-      mongoose.set("debug", true);
-    }
+      if (process.env.NODE_ENV) {
+        mongoose.set("debug", true);
+      }
 
-    await mongoose.connect(process.env.MONGO_URL, connectionOptions);
-    this.retryCount = 0;
+      await mongoose.connect(process.env.MONGO_URL, connectionOptions);
+      this.retryCount = 0;
+    } catch (error) {
+      console.error(error.message);
+      await this.handleConnectionError();
+    }
+  }
+
+  async handleConnectionError() {
+    if (this.retryCount < MAX_RETRIES) {
+      this.retryCount++;
+      console.log(
+        `RETRYING connection .. ATTEMPT ${this.retryCount} of ${MAX_RETRIES}`
+      );
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          resolve;
+        }, RETRY_INTERVAL)
+      );
+      return this.connect();
+    } else {
+      console.error(`Failed to connect to DB ${MAX_RETRIES} attempt`);
+      process.exit(1);
+    }
+  }
+
+  async handleDisconnect() {
+    if (!this.isConnected) {
+      console.log("Attempting to reconnect to DB..");
+      this.connect();
+    }
+  }
+
+  async handleAppTermination() {
+    try {
+      await mongoose.connection.close();
+      console.log("Mongodb connection close through app termination");
+      process.exit(1);
+    } catch (error) {
+      console.log("Error during database disconnection");
+      process.exit(1);
+    }
   }
 }
