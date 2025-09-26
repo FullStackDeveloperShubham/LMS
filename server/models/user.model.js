c9aaf7a285648840b936fb194b5c3fa8a031490c
FullStackDeveloperShubham <shubhamfullstackdeveloper@gmail.com>
2025-09-26 22:09:29 +0530

    User model defined

---
+++ server/models/user.model.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxLength: [50, "Name cannot exit 50 character"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      lowerCase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please provide a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be atleast 8 character"],
+      select: false,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please provide a valid email",
+      ],
+    },
+    roal: {
+      type: String,
+      enum: {
+        values: ["student", "instructor", "admin"],
+        message: "Please select a valid roal",
+      },
+      default: "student",
+    },
+    avatar: {
+      type: String,
+      default: "default-avatar.png",
+    },
+    bio: {
+      type: String,
+      maxLength: [200, "Bio cannont exid to 200 character"],
+    },
+    enrolledCourses: [
+      {
+        course: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Course",
+        },
+        enrolledAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    createdCourse: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    resetPasswordToken: String,
+    resetPasswordExpiry: Date,
+    lastActive: {
+      type: Date,
+      default: Date.nw,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
