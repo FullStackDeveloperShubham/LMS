2b003d8338f34b3d1afb98db7d02bf488279d4b4
FullStackDeveloperShubham <shubhamfullstackdeveloper@gmail.com>
2025-09-27 18:48:30 +0530

    course progress schema designed

--- server/models/courseProgress.js
+++ server/models/courseProgress.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// lecture progress schema
 const lectureProgressSchema = new mongoose.Schema({
   lecture: {
     type: mongoose.Schema.Types.ObjectId,
@@ -19,3 +20,39 @@ const lectureProgressSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+// course progress schema
+const courseProgressSCheam = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "User refrence is required"],
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: [true, "Course refrence is required"],
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    completionPercentage: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 100,
+    },
+    lectureProgress: [lectureProgressSchema],
+    lastAccessed: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
